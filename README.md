d5780e9401b35f228710584518ee9401509b913d
caoyankui <yankui.cao@andall.com>
2019-08-20 10:34:53 +0800

    graphql

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # graphql
+
+https://graphql.cn/learn/
+
+https://graphql.cn/graphql-js/graphql-clients/
+
+https://mp.weixin.qq.com/s/PovMmO6ddcpBO5A2WtY89Q
---
+++ hello.js
@@ -0,0 +1,20 @@
+var { graphql, buildSchema } = require('graphql');
+
+// 使用 GraphQL schema language 构建一个 schema
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// 根节点为每个 API 入口端点提供一个 resolver 函数
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+// 运行 GraphQL query '{ hello }' ，输出响应
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
