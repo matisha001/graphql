3774e331aa7ec3a39dc8213001357ea09b7e5b09
caoyankui <yankui.cao@andall.com>
2019-08-20 13:33:17 +0800

    graphql

---
+++ demo/t2_传递参数.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var graphqlHTTP = require('express-graphql')
+var { buildSchema } = require('graphql')
+
+// 使用 GraphQL schema language 构造一个 schema
+var schema = buildSchema(`
+  type Query {
+    rollDice(numDice: Int!, numSides: Int): [Int]
+  }
+`)
+
+// root 为每个端点入口 API 提供一个解析器
+var root = {
+  rollDice: function({ numDice, numSides }) {
+    var output = []
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+    }
+    return output
+  }
+}
+
+var app = express()
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+)
+app.listen(4000)
+console.log('Running a GraphQL API server at localhost:4000/graphql')
